fbe3aeb291b9a28fedbce96ee246740af62ae3d8
EvgeniOrmotsadze <eormo11@freeuni.edu.ge>
2014-06-22 05:51:59 -0700

    context listener add, web.xml mapping

---
+++ src/ge/freeuni/restaurant/controllers/ContextListener.java
@@ -0,0 +1,56 @@
+package ge.freeuni.restaurant.controllers;
+
+import java.util.HashMap;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ContextListener
+ * 
+ */
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+	/**
+	 * Default constructor.
+	 */
+	public ContextListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent arg0) {
+		HashMap<Integer, String> typeMap = new HashMap<Integer, String>();
+		HashMap<Integer, String> locationMap = new HashMap<Integer, String>();
+		
+		typeMap.put(1, "რესტორანი");
+		typeMap.put(2, "ბარი");
+		typeMap.put(3, "კაფე");
+		typeMap.put(4, "სწრაფი კვება");
+		typeMap.put(5, "პაბი");
+		typeMap.put(6, "კლუბი");
+		
+		locationMap.put(1, "საბურთალო");
+		locationMap.put(2, "ვაკე");
+		locationMap.put(3, "მთაწმინდა");
+		locationMap.put(4, "დიდუბე");
+		locationMap.put(5, "ვარკეთილი");
+		locationMap.put(6, "ზაჰესი");
+		
+		arg0.getServletContext().setAttribute("TypeMap", typeMap);
+		arg0.getServletContext().setAttribute("LocationMap", locationMap);
+
+	}
+
+}
