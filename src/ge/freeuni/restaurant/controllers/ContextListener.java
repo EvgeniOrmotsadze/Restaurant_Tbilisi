92d057feee31f2d93dc008a04afc483140be4284
datoAbesadze <dabes11@freeuni.edu.ge>
2014-06-24 20:45:09 +0400

    contextListener update, web.xml fix

--- src/ge/freeuni/restaurant/controllers/ContextListener.java
+++ src/ge/freeuni/restaurant/controllers/ContextListener.java
@@ -1,5 +1,8 @@
 package ge.freeuni.restaurant.controllers;
 
+import ge.freeuni.restaurant.service.CurrencyAutoUpdater;
+import ge.freeuni.restaurant.service.CurrencyProvider;
+
 import java.util.HashMap;
 
 import javax.servlet.ServletContextEvent;
@@ -10,55 +13,57 @@ import javax.servlet.annotation.WebListener;
 
 /**
  * Application Lifecycle Listener implementation class ContextListener
- *
+ * 
  */
 @WebListener
-public class ContextListener implements ServletContextListener, ServletRequestAttributeListener {
-
-    /**
-     * Default constructor. 
-     */
-    public ContextListener() {
-        // TODO Auto-generated constructor stub
-    }
+public class ContextListener implements ServletContextListener,
+		ServletRequestAttributeListener {
 
 	/**
-     * @see ServletRequestAttributeListener#attributeRemoved(ServletRequestAttributeEvent)
-     */
-    public void attributeRemoved(ServletRequestAttributeEvent arg0) {
-        // TODO Auto-generated method stub
-    }
+	 * Default constructor.
+	 */
+	public ContextListener() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent arg0) {
-        // TODO Auto-generated method stub
-    }
+	 * @see ServletRequestAttributeListener#attributeRemoved(ServletRequestAttributeEvent)
+	 */
+	public void attributeRemoved(ServletRequestAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
 
 	/**
-     * @see ServletRequestAttributeListener#attributeAdded(ServletRequestAttributeEvent)
-     */
-    public void attributeAdded(ServletRequestAttributeEvent arg0) {
-        // TODO Auto-generated method stub
-    }
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+	}
 
 	/**
-     * @see ServletRequestAttributeListener#attributeReplaced(ServletRequestAttributeEvent)
-     */
-    public void attributeReplaced(ServletRequestAttributeEvent arg0) {
-        // TODO Auto-generated method stub
-    }
+	 * @see ServletRequestAttributeListener#attributeAdded(ServletRequestAttributeEvent)
+	 */
+	public void attributeAdded(ServletRequestAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
 
 	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent arg0) {
-    	HashMap<Integer, String> typeMap = new HashMap<Integer, String>();
-		HashMap<Integer, String> locationMap = new HashMap<Integer, String>();	
-		
+	 * @see ServletRequestAttributeListener#attributeReplaced(ServletRequestAttributeEvent)
+	 */
+	public void attributeReplaced(ServletRequestAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent arg0) {
+		HashMap<Integer, String> typeMap = new HashMap<Integer, String>();
+		HashMap<Integer, String> locationMap = new HashMap<Integer, String>();
+
 		arg0.getServletContext().setAttribute("TypeMap", typeMap);
 		arg0.getServletContext().setAttribute("LocationMap", locationMap);
-    }
-	
+		CurrencyAutoUpdater.start(900);
+	}
+
 }
