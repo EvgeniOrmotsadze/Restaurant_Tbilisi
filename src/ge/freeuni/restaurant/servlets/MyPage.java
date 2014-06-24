3cfe6395e8e198959be9f15a3d94016d5aba25fd
datoAbesadze <dabes11@freeuni.edu.ge>
2014-06-25 02:17:16 +0400

    paging servlet

--- src/ge/freeuni/restaurant/servlets/MyPage.java
+++ src/ge/freeuni/restaurant/servlets/MyPage.java
@@ -1,6 +1,5 @@
 package ge.freeuni.restaurant.servlets;
 
-
 import ge.freeuni.restaurant.controllers.DBQuery;
 import ge.freeuni.restaurant.model.Restaurant;
 import ge.freeuni.restaurant.model.User;
@@ -22,49 +21,64 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MyPage")
 public class MyPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MyPage() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		User user = (User)request.getSession().getAttribute("user");
-		if(user == null){
-			RequestDispatcher dispatcher = request.getRequestDispatcher("user-login.jsp");
+	public MyPage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String pageNumber = getPageNumber(request);
+
+		User user = (User) request.getSession().getAttribute("user");
+		if (user == null) {
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("user-login.jsp");
 			dispatcher.forward(request, response);
-		}else{
+		} else {
 			DBQuery db = new DBQuery();
 			ArrayList<Restaurant> res = new ArrayList<Restaurant>();
 			try {
-				res = db.getMyRestaurants(user.getID());
+				res = db.getMyRestaurants(user.getID(),
+						Integer.parseInt(pageNumber));
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			
+
 			request.setAttribute("MyRestaurants", res);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("mypage.jsp");
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("mypage.jsp?pageNumber=" + pageNumber);
 			dispatcher.forward(request, response);
 		}
-		
-		
+
+	}
+
+	// returning page number
+	private String getPageNumber(HttpServletRequest request) {
+		if (request.getParameter("pageNumber").equals(null))
+			return "1";
+		return Integer.toString(Integer.parseInt(request
+				.getParameter("pageNumber")) + 1);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request,response);
-		
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+
 	}
 
 }
